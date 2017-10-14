6f83c39c0b8426e96e17aec99f9caf5171eae654
Samuel Zhang <samuelzhang3@gmail.com>
2017-10-13 22:47:21 -0400

    google api key loaded

--- app/src/main/java/hackgt/food_mood/MainActivity.java
+++ app/src/main/java/hackgt/food_mood/MainActivity.java
@@ -5,6 +5,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Reads Google Places API Key from "places_api_key.txt"
+    String fileName = "places_api_key.txt";
+    String placesApiKey = "";
+    try {
+        // FileReader reads text files in the default encoding.
+        FileReader file =
+            new FileReader(fileName);
+
+        // Always wrap FileReader in BufferedReader.
+        BufferedReader bufferedReader =
+            new BufferedReader(fileReader);
+
+        placesApiKey = bufferedReader.readLine();
+        bufferedReader.close();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
